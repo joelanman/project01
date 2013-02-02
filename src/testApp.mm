e00df5fd9d6f70fc7afff8f317a3424739bd13e8
Gerard Rallo <gerardrallo@gmail.com>
2013-02-02 18:53:45 +0000

    it might be broken

--- src/testApp.mm
+++ src/testApp.mm
@@ -6,21 +6,51 @@ void testApp::setup(){
     ofRegisterTouchEvents(this);
 	// initialize the accelerometer
 	ofxAccelerometer.setup();
-	
-	//If you want a landscape oreintation 
-	//iPhoneSetOrientation(OFXIPHONE_ORIENTATION_LANDSCAPE_RIGHT);
-	
-	ofBackground(127,127,127);
+    
+    ofxiPhoneAlerts.addListener(this);
+	ofSetFrameRate(60);
+	ofBackgroundHex(0xfdefc2);
+    
+    
+    box2d.init();
+    box2d.setGravity(0, 10);
+    box2d.setFPS(60);
+    box2d.registerGrabbing();
+    
+    ofRectangle bounds(0, 0, 768, 1500);
+    box2d.createBounds(bounds);
+    box2d.setIterations(1, 1); // minimum for IOS
+    
+    for (int i=0; i < 5; i++) {
+        for(int j = 0; j < 10; j++){
+            ofxBox2dCircle c;
+            c.setPhysics(1, 0.4, 0.4);
+            c.setup(box2d.getWorld(), ofRandomWidth(), ofRandomHeight(), ofRandom(13, 25));
+            circles.push_back(c);
+        }
+    }
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-
+    ofVec2f gravity = ofxAccelerometer.getForce();
+    gravity.y *= -1;
+    gravity *= 30;
+    box2d.setGravity(gravity);
+    
+    box2d.update();
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-	
+    ofSetHexColor(0xABDB44);
+    for(vector<ofxBox2dCircle>::iterator it = circles.begin(); it != circles.end(); ++it) {
+        it->draw();
+    }
+    
+    ofSetColor(90);
+    ofDrawBitmapString("double tap to add more", 20, 30);
+    ofDrawBitmapString(ofToString(ofGetFrameRate(), 0)+" fps", 20, 50);
 }
 
 //--------------------------------------------------------------
@@ -45,7 +75,10 @@ void testApp::touchUp(ofTouchEventArgs & touch){
 
 //--------------------------------------------------------------
 void testApp::touchDoubleTap(ofTouchEventArgs & touch){
-
+    ofxBox2dCircle c;
+    c.setPhysics(1, 0.4, 0.4);
+    c.setup(box2d.getWorld(), touch.x, touch.y, ofRandom(13, 25));
+    circles.push_back(c);
 }
 
 //--------------------------------------------------------------
